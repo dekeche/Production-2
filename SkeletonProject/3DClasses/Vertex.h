ad1bca9559ea6ce602057186005ef380bca0dd36
revix4 <james.p1@verizon.net>
2015-03-01 16:26:38 -0500

    added fx files

--- SkeletonProject/3DClasses/Vertex.h
+++ SkeletonProject/3DClasses/Vertex.h
@@ -14,11 +14,19 @@ void DestroyAllVertexDeclarations();
 //=============================================================================
 struct VertexPos
 {
-	VertexPos():pos(0.0f, 0.0f, 0.0f){}
-	VertexPos(float x, float y, float z):pos(x,y,z){}
-	VertexPos(const D3DXVECTOR3& v):pos(v){}
+	VertexPos():pos(0.0f, 0.0f, 0.0f),
+	normal(0.0f,0.0f,0.0f),
+	tex0(0.0f,0.0f){}
+	VertexPos(float x, float y, float z,
+		float nx, float ny, float nz,
+		float u, float v):pos(x,y,z),normal(nx,ny,nz),tex0(u,v){}
+	VertexPos(const D3DXVECTOR3& v,
+		const D3DXVECTOR3& n,
+		const D3DXVECTOR2& uv) :pos(v),normal(n),tex0(uv){}
 
 	D3DXVECTOR3 pos;
+	D3DXVECTOR3 normal;
+	D3DXVECTOR2 tex0;
 	static IDirect3DVertexDeclaration9* Decl;
 };
 //=============================================================================
