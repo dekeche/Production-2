36ae784b418d62e723f0ebfea42dd8a8f55932a3
revix4 <james.p1@verizon.net>
2015-03-31 21:43:46 -0400

    Forgot

---
+++ SkeletonProject/EnhancedMaterial.h
@@ -0,0 +1,25 @@
+//=============================================================================
+//                              BaseMaterial
+//
+// Writen by Jesse McKinley, 2015
+// EGP 300-101, Graphics Programming II  - skeleton project
+//
+// Enhanced class of BaseMaterial, used for Assignment 4
+//=============================================================================
+#pragma once
+#include "d3dApp.h"
+//=============================================================================
+class EnhancedMaterial
+{
+protected:
+
+	D3DXHANDLE          m_WorldMatHandle;
+
+
+public:
+	EnhancedMaterial(void);
+	virtual ~EnhancedMaterial(void);
+
+};
+//=============================================================================
+
